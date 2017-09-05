a287a67bbd9f79eef2f12373e5fdc59ebacad801
Joeny Bui <joeny.bui@gmail.com>
2017-09-04 23:57:46 -0500

    Update part 1.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,12 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 export class AppComponent {
   items: FirebaseListObservable<any[]>;
   title = 'app';
+  fullName = 'Joeny Bui';
+  jobTitle = 'Software Developer';
+  email = 'joeny.bui@gmail.com';
+  about = "I'm a scientific software developer with a background in structural engineering. I'm interested in all " +
+    "aspects of programming, but my current focus is in machine learning, big data, and full-stack web development.";
+  website = 'https://www.joenybui.com';
 
   constructor(db: AngularFireDatabase) {
     this.items = db.list('/items');
