3b43857dfbf823fae689157d92ba755dc08f1a33
Joeny Bui <joeny.bui@gmail.com>
2017-11-07 11:35:12 -0600

    Added blog component.

--- src/app/blog.component.ts
+++ src/app/blog.component.ts
@@ -1,17 +1,18 @@
 import {Component, OnInit} from "@angular/core";
 import {Router} from '@angular/router';
 
-import {WritingComponent} from './writing.component';
+import { Blog } from './blog';
 
 @Component({
   selector: 'blog',
   templateUrl: './blog.component.html'
 })
 export class BlogComponent implements OnInit {
+  bodyHtml = "";
 
   constructor(private router: Router) {
+    this.bodyHtml = "<b>Testing</b>"
   }
-
 
   ngOnInit(): void {
 
